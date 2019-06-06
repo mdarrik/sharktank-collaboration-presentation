909da20b3ba193c34ba13ebaffae7b10e417708d
Darrik Moberg <MOBY235@walni.onmicrosoft.com>
2019-06-06 12:14:03 -0700

    chore: add eslint-plugin-vue-a11y

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+  extends: [
+    "plugin:vue/essential",
+    "@vue/prettier",
+    "@vue/typescript",
+    "plugin:vue-a11y/base"
+  ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
