d7c24b0c2a0ceedb36e31684c152ac294e56f02b
Darrik Moberg <MOBY235@walni.onmicrosoft.com>
2019-06-06 16:29:17 -0700

    refactor: Separate layout into own component.
    Also adds router outlet

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,13 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld />
-    </v-content>
-  </v-app>
+  <layout>
+    <router-view></router-view>
+  </layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import Layout from './components/Layout'
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data() {
-    return {
-      //
-    }
-  },
+  components: { Layout },
 }
 </script>
---
+++ src/components/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <header>
+      <v-toolbar clipped-left app absolute>
+        <v-toolbar-title>L&I IT Shark Tank Presentation</v-toolbar-title>
+      </v-toolbar>
+    </header>
+    <v-content>
+      <v-container fluid tag="main">
+        <slot></slot>
+      </v-container>
+    </v-content>
+    <v-footer inset app>
+      <span class="px-3">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
