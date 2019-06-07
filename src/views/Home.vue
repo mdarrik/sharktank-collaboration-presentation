49c1f612290703ea3e7027d8a9456e179de2049e
Darrik Moberg <MOBY235@walni.onmicrosoft.com>
2019-06-07 07:48:09 -0700

    refactor: changes home page to a splash screen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <v-layout align-center justify-center>
+    <v-flex xs10>
+      <v-card>
+        <v-img
+          :src="require('../assets/undraw_software_engineer_lvl5.svg')"
+          contain
+          height="300px"
+        ></v-img>
+        <v-card-title class="d-block text-xs-center">
+          <h1>Improving Developer Collaboration</h1>
+          <h2>By Isaac Ortiz, Darrik Moberg, & Ken Grubb</h2>
+        </v-card-title>
+        <v-card-actions>
+          <v-layout row wrap align-center justify-center>
+            <v-btn to="/Presentation">Begin</v-btn>
+          </v-layout>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default Vue.extend({
   name: 'home',
-  components: {
-    HelloWorld,
-  },
 })
 </script>
