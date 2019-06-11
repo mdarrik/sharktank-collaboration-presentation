842dd7e4a9437c3cdeb9574d5ead7e60a132f2e9
Darrik Moberg <MOBY235@walni.onmicrosoft.com>
2019-06-11 15:41:31 -0700

    refactor: add image to question slide

--- src/components/slides/QuestionsSlide.vue
+++ src/components/slides/QuestionsSlide.vue
@@ -2,6 +2,12 @@
   <presentation-step-layout>
     <template v-slot:slide-heading>
       <h2 class="display-4 mb-5">Questions?</h2>
+      <v-img
+        :src="require('../../assets/undraw_questions.svg')"
+        contain
+        height="500px"
+        width="600px"
+      ></v-img>
     </template>
   </presentation-step-layout>
 </template>
