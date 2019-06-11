6686e445f731eb806ee6de89cb8ef79213149485
Darrik Moberg <MOBY235@walni.onmicrosoft.com>
2019-06-11 11:11:38 -0700

    feature: Adds basic template for slides

---
+++ src/components/PresentationStepLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-layout tag="section" column align-center justify-center xs10>
+    <slot name="slide-heading"></slot>
+    <slot></slot>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'PresentationStepLayout',
+})
+</script>
