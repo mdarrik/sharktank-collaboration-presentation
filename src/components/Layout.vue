f8209a862050e39138aecf7e39be0be496ca0044
Darrik Moberg <MOBY235@walni.onmicrosoft.com>
2019-06-12 12:59:48 -0700

    fix: add attributions.
    Also adds image to title

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -2,7 +2,20 @@
   <v-app>
     <header>
       <v-toolbar clipped-left app absolute class="primary white--text">
-        <v-toolbar-title>L&I IT Shark Tank Presentation</v-toolbar-title>
+        <v-toolbar-title>
+          <v-img
+            :src="require('../assets/shark.svg')"
+            width="2.5em"
+            height="2.5em"
+            class="d-inline-block"
+          ></v-img>
+          <router-link
+            active-class
+            to="/"
+            class="ml-4 mb-auto mt-auto white--text"
+            >L&I IT Shark Tank Presentation</router-link
+          >
+        </v-toolbar-title>
       </v-toolbar>
     </header>
     <v-content>
@@ -10,8 +23,42 @@
         <slot></slot>
       </v-container>
     </v-content>
-    <v-footer inset app>
+    <v-footer app class="d-flex row justify-space-between" height="auto">
       <span class="px-3">&copy; {{ new Date().getFullYear() }}</span>
+      <v-layout tag="section" class="caption" column shrink>
+        <h2 class="caption bold">Attributions</h2>
+        <cite>
+          Floating Cogs: Steve Schoger's
+          <a href="https://www.heropatterns.com">Hero Patterns</a>
+          <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>
+        </cite>
+        <cite class="grow">
+          Large Splash Images from Katerina Limpitsouni's
+          <a href="https://undraw.co">unDraw</a>
+        </cite>
+        <div>
+          Shark Icon made by
+          <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from
+          <a href="https://www.flaticon.com/" title="Flaticon"
+            >www.flaticon.com</a
+          >
+          is licensed by
+          <a
+            href="http://creativecommons.org/licenses/by/3.0/"
+            title="Creative Commons BY 3.0"
+            target="_blank"
+            >CC 3.0 BY</a
+          >
+        </div>
+      </v-layout>
     </v-footer>
   </v-app>
 </template>
+<style scoped>
+footer cite {
+  font-style: normal;
+}
+.v-toolbar__title a {
+  text-decoration: none;
+}
+</style>
