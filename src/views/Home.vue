199bc2300d04fdc62077757c3a84184a2e06619f
Darrik Moberg <MOBY235@walni.onmicrosoft.com>
2019-06-13 15:13:26 -0700

    refactor: remove autofocus from v-btn link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,8 +14,7 @@
         </v-card-title>
         <v-card-actions>
           <v-layout row wrap align-center justify-center>
-            <!-- eslint-disable vue-a11y/no-autofocus-->
-            <v-btn to="/Presentation" autofocus>Begin</v-btn>
+            <v-btn to="/Presentation" ref="presentationLink">Begin</v-btn>
           </v-layout>
         </v-card-actions>
       </v-card>
