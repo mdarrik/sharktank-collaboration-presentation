8c6ab203db909e36005c8246fef99b6b0260c283
Darrik Moberg <MOBY235@walni.onmicrosoft.com>
2019-06-13 14:22:42 -0700

    Refactor: Adds more information to the How slide

--- src/components/slides/HowWeGetItSlide.vue
+++ src/components/slides/HowWeGetItSlide.vue
@@ -3,26 +3,41 @@
     <template v-slot:slide-heading>
       <h2 class="display-3 mb-5">How We&apos;ll Get It</h2>
     </template>
-    <ul class="display-2">
-      <li>Use a Discussion/Q&amp;A Forum</li>
-      <li>COTS</li>
-      <li>
-        Options:
-        <dl>
-          <dt>
-            <a href="https://stackoverflow.com/">Stack Overflow</a>
-          </dt>
-          <dd><strong>$17</strong> per user/month</dd>
-          <dt>
-            <a href="https://www.discourse.org/">Discourse</a>
-          </dt>
-          <dd><strong>$0</strong> per month (Self-Hosted)</dd>
-          <dt>
-            <a href="https://dev.to">Dev.TO</a>
-          </dt>
-          <dd><strong>$0</strong> per month (Self-Hosted)</dd>
-        </dl>
-      </li>
+    <ul>
+      <dl>
+        <dt class="display-1">
+          <a href="https://stackoverflow.com/">Stack Overflow</a>
+        </dt>
+        <dd class="title">
+          <ul>
+            <li><strong>$17</strong> per user/month</li>
+            <li>Most Familiar (> 1,000,000 users)</li>
+            <li>Only High Cost Options Available</li>
+          </ul>
+        </dd>
+        <dt class="display-1">
+          <a href="https://www.discourse.org/">Discourse</a>
+        </dt>
+        <dd class="title">
+          <ul>
+            <li>
+              <strong>$0</strong>
+              per month (Self-Hosted)
+            </li>
+            <li>Familiar Format &amp; Free to use</li>
+          </ul>
+        </dd>
+        <dt class="display-1">
+          <a href="https://dev.to">Dev.TO</a>
+        </dt>
+        <dd class="title">
+          <ul>
+            <li><strong>$0</strong> per month (Self-Hosted)</li>
+            <li>Well Designed for longer posts like tutorials</li>
+            <li>Not Strictly a Q&amp;A Forum</li>
+          </ul>
+        </dd>
+      </dl>
     </ul>
   </presentation-step-layout>
 </template>
@@ -38,16 +53,19 @@ export default Vue.extend({
 <style>
 dl {
   padding-left: 3rem;
-  margin-top: 2rem;
   display: grid;
   grid-template-columns: [dt] 45% [dd] 65%;
   grid-template-rows: 1fr 1fr 1fr;
+  grid-row-gap: 5px;
 }
 dt {
   display: list-item;
   list-style-image: url('../../assets/star-outline.svg');
 }
-dd {
-  display: inline-block;
+dd ul {
+  list-style: none;
+}
+dd ul li {
+  margin-bottom: 0.8em;
 }
 </style>
